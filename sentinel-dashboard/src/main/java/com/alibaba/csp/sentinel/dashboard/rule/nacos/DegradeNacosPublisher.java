7ba5013933d82658c043126c1b9784a2b1b898b9
dingran <m17611378973@163.com>
2024-02-19 14:11:34 +0800

    Initial commit

---
+++ sentinel-dashboard/src/main/java/com/alibaba/csp/sentinel/dashboard/rule/nacos/DegradeNacosPublisher.java
@@ -0,0 +1,33 @@
+package com.alibaba.csp.sentinel.dashboard.rule.nacos;
+
+import com.alibaba.csp.sentinel.dashboard.datasource.entity.rule.DegradeRuleEntity;
+import com.alibaba.csp.sentinel.dashboard.datasource.entity.rule.FlowRuleEntity;
+import com.alibaba.csp.sentinel.dashboard.rule.DynamicRulePublisher;
+import com.alibaba.csp.sentinel.datasource.Converter;
+import com.alibaba.csp.sentinel.util.AssertUtil;
+import com.alibaba.nacos.api.config.ConfigService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@Component("degradeRuleNacosPublisher")
+public class DegradeNacosPublisher implements DynamicRulePublisher<List<DegradeRuleEntity>> {
+
+    @Autowired
+    private ConfigService configService;
+
+    @Autowired
+    private Converter<List<DegradeRuleEntity>, String> converter;
+
+    @Override
+    public void publish(String app, List<DegradeRuleEntity> rules) throws Exception {
+        AssertUtil.notEmpty(app, "app name cannot be empty");
+        if (rules == null) {
+            return;
+        }
+        configService.publishConfig(app + NacosConfigUtil.DEGRADE_DATA_ID_POSTFIX,
+                NacosConfig.group, converter.convert(rules));
+    }
+}
